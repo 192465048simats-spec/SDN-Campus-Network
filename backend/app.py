033ad804f1ac40445ca1517294ab4c0163f92062
POTHURAJU SAI CHARAN TEJ <192465048.simats@gmail.com>
2026-08-20 11:27:41 +0530

    Prepare project for deployment

--- backend/app.py
+++ backend/app.py
@@ -1250,8 +1250,8 @@ if __name__ == "__main__":
     print("--------------------------------------")
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True,
-        use_reloader=False
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=False,
+    use_reloader=False
     )
